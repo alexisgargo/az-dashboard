5d9b902bb284aac5090bf90a3af20237fb1ab3ba
Marciram <106172470+Marciram@users.noreply.github.com>
2024-04-12 12:12:45 -0600

    Frontend para prueba de arquitectura

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,45 +1,68 @@
-import Image from "next/image";
+'use client';
+import { useState } from 'react';
+import {Button, ButtonGroup,Table} from "@nextui-org/react";
 
-export default function Home() {
+import {   TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/react";
+
+interface IssueType {
+  key: string;
+  status: string;
+  description: string;
+  author: string;
+  summary: string;
+  assignee: string;
+}
+const baseUrl = 'http://localhost:8080';
+const url = new URL('project/DAS', baseUrl);
+export default function Page() {
+  const [issues, setIssues] = useState<IssueType[]>([]);
+
+  const fetchIssues = async () => {
+    console.log("Button Issues pressed!")
+    try {
+      const response = await fetch(url); 
+      const data = await response.json();
+      setIssues(data);
+    } catch (error) {
+      console.error('Error fetching Jira issues:', error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+    <Button onPress={() => console.log("Button pressed!")}>
+      Click Me
+    </Button>
+
+    <Button onPress={fetchIssues}>Load Jira Issues</Button>
+    <Table
+      aria-label="Jira Issues"
+      
+    >
+      <TableHeader>
+        <TableColumn>Key</TableColumn>
+        <TableColumn>Status</TableColumn>
+        <TableColumn>Description</TableColumn>
+        <TableColumn>Author</TableColumn>
+        <TableColumn>Summary</TableColumn>
+        <TableColumn>Assignee</TableColumn>
+      </TableHeader>
+      <TableBody>
+        {issues.map((issue) => (
+          <TableRow key={issue.key}>
+            <TableCell>{issue.key}</TableCell>
+            <TableCell>{issue.status}</TableCell>
+            <TableCell>{issue.description}</TableCell>
+            <TableCell>{issue.author}</TableCell>
+            <TableCell>{issue.summary}</TableCell>
+            <TableCell>{issue.assignee}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
