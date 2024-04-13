494f32643f821b19076578dff81a35bb47a09fd2
Giuliana <gherreralpez@gmail.com>
2024-04-12 22:38:40 -0600

    AddInputs

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,7 +1,37 @@
-import Image from "next/image";
+'use client'
+import {Input} from "@nextui-org/react";
+import {Dropdown, DropdownMenu, DropdownTrigger, DropdownItem, Button} from "@nextui-org/react";
 
-export default function Home() {
+import { useState } from 'react'
+export default function App() {
+  const [value, setValue] = useState("");
+  const [value1, setValue1] = useState([]);
   return (
-   <h1>Hola</h1>
+    <div className="w-full flex flex-col gap-2 max-w-[240px]">
+      <Input
+        label="Email"
+        placeholder="Enter your email"
+        value={value}
+        onValueChange={setValue}
+      />
+      <p className="text-default-500 text-small">Input value: {value}</p>
+      <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered">Open Menu</Button>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Link Actions">
+            <DropdownItem key="home" href="/home">
+              {value}
+            </DropdownItem>
+            <DropdownItem key="about" href="/about">
+              About
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+    </div>
+   
+       
+     
   );
 }
+
