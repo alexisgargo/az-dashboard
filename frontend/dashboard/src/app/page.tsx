9e2d273e683105e7ed5ce78a02466bf60859e450
Giuliana <gherreralpez@gmail.com>
2024-04-14 11:20:36 -0600

    AddDropwdown

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -13,7 +13,6 @@ export default function App() {
         value={value}
         onValueChange={setValue}
       />
-      
       <div className="inline-block relative w-64">
         <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
           <option>Engeenirs</option>
