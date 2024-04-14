755ed9f8bee99c24b8d8108b1aacbe9c4cdd244c
Giuliana <gherreralpez@gmail.com>
2024-04-14 10:35:45 -0600

    AddDropwdown

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,37 +1,32 @@
 'use client'
-import {Input} from "@nextui-org/react";
-import {Dropdown, DropdownMenu, DropdownTrigger, DropdownItem, Button} from "@nextui-org/react";
-
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { useState } from 'react'
 export default function App() {
   const [value, setValue] = useState("");
-  const [value1, setValue1] = useState([]);
+ 
   return (
-    <div className="w-full flex flex-col gap-2 max-w-[240px]">
+    <div >
       <Input
-        label="Email"
-        placeholder="Enter your email"
+        label="Engeenir"
+        placeholder="Enter your Engeenir"
         value={value}
         onValueChange={setValue}
       />
-      <p className="text-default-500 text-small">Input value: {value}</p>
-      <Dropdown>
-          <DropdownTrigger>
-            <Button variant="bordered">Open Menu</Button>
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Link Actions">
-            <DropdownItem key="home" href="/home">
-              {value}
-            </DropdownItem>
-            <DropdownItem key="about" href="/about">
-              About
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+      
+      <div className="inline-block relative w-64">
+        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+          <option>Engeenirs</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+        </select>
+
+      </div>
+
     </div>
-   
-       
-     
+
+
+
   );
 }
 
