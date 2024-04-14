7aba35dfff7e436b70e06f39dcaa36580ff8fc8d
Giuliana <gherreralpez@gmail.com>
2024-04-14 17:59:05 -0600

    MethodPost

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,30 +1,59 @@
 'use client'
-import { Input } from "@nextui-org/react";
-import { useState } from 'react'
-export default function App() {
-  const [value, setValue] = useState("");
- 
-  return (
-    <div >
-      <Input
-        label="Engeenir"
-        placeholder="Enter your Engeenir"
-        value={value}
-        onValueChange={setValue}
-      />
-      <div className="inline-block relative w-64">
-        <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-          <option>Engeenirs</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-        </select>
+import { useState } from 'react';
+import { Button } from "@nextui-org/react";
 
-      </div>
-
-    </div>
-
-
-
-  );
+interface Engineer {
+  name: string;
+  id: number;
 }
 
+const baseUrl = 'http://localhost:8080/az_dashboard/engineers';
+
+export default function Page() {
+  const [engineers, setEngineers] = useState<Engineer[]>([]);
+  const [name, setName] = useState('');
+  const [id, setId] = useState('');
+
+  const handleAddEngineer = async () => {
+    try {
+      const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, id })
+      });
+  
+      if (response.ok) {
+        const newEngineer = await response.json();
+        setEngineers([...engineers, newEngineer]);
+        setName('');
+        setId('');
+      } else {
+        console.error('Error adding engineer:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding engineer:', error);
+    }
+  };
+  
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="ID"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <Button onClick={handleAddEngineer}>Add Engineer</Button>
+     
+    </div>
+  );
+}
