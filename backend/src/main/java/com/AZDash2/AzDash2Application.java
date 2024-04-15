b7d798c7dd3c15d809e4dbe30e774960f5ec7d35
Giuliana <gherreralpez@gmail.com>
2024-04-14 21:08:07 -0600

    GetIssues

--- backend/src/main/java/com/AZDash2/AzDash2Application.java
+++ backend/src/main/java/com/AZDash2/AzDash2Application.java
@@ -5,19 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class AzDash2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzDash2Application.class, args);
 	}
+
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-                registry.addMapping("/az_dashboard/engineer").allowedOrigins("http://localhost:3000");
-            }
-        };
-    }
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/az_dashboard/engineer")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST") // Permitir métodos GET y POST
+						.allowCredentials(true); // Permitir credenciales (por ejemplo, cookies)
+
+				registry.addMapping("/az_dashboard/issues")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET"); // Permitir solo el método GET para esta ruta
+			}
+		};
+	}
+
 }
