84d5814c87c64e955192ca638adc6cb08da77529
Giuliana <gherreralpez@gmail.com>
2024-04-14 20:25:27 -0600

    AddTailwind

--- frontend/dashboard/src/app/page.tsx
+++ frontend/dashboard/src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 import { useState } from 'react';
-import { Button } from "@nextui-org/react";
-import axios from 'axios'; 
+import {  Avatar, Button, NextUIProvider } from "@nextui-org/react";
+import axios from 'axios';
 
 interface Engineer {
   name: string;
   id: number;
 }
-
 const baseUrl = 'http://localhost:8080/az_dashboard/engineer';
 
 export default function App() {
@@ -20,8 +19,8 @@ export default function App() {
         headers: {
           'Content-Type': 'application/json'
         }
-      }); // Utilizar Axios para hacer la solicitud POST
-      
+      });
+
       if (response.status === 200) {
         const newEngineer = response.data;
         setEngineers([...engineers, newEngineer]);
@@ -33,18 +32,23 @@ export default function App() {
       console.error('Error adding engineer:', error);
     }
   };
-  
-
   return (
-    <div>
+  
+    <NextUIProvider>
+      <div className="w-72">
+      <div className="relative w-full min-w-[200px] h-10">
       <input
         type="text"
         placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        className="max-w-[220px]"
       />
       <Button onClick={handleAddEngineer}>Add Engineer</Button>
-     <h1>{name}</h1>
+      <h1>{name}</h1>
+      </div>
+    
     </div>
+  </NextUIProvider>
   );
 }
