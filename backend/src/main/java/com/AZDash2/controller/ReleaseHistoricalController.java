acbc42e9cd55de82e799e4fcc754817f9b9ac16a
SalvadoRobles03 <99054999+SalvadoRobles03@users.noreply.github.com>
2024-04-18 20:51:12 -0600

    📦 NEW: MAC-21 | Release Historical Controller

---
+++ backend/src/main/java/com/AZDash2/controller/ReleaseHistoricalController.java
@@ -0,0 +1,34 @@
+package com.AZDash2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.AZDash2.service.ReleaseHistoricalService;
+import com.AZDash2.entity.ReleaseHistorical;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.sql.Date;
+
+
+@RestController
+@RequestMapping("az_dashboard")
+public class ReleaseHistoricalController {
+
+    @Autowired
+    private ReleaseHistoricalService releaseHistoricalService;
+
+    @GetMapping("/historical/{date}/{idRelease}")
+    public ResponseEntity<ReleaseHistorical> getIssuesByDateAndRelease(@PathVariable("date") Date date,
+            @PathVariable("idRelease") Long idRelease) {
+        ReleaseHistorical record;
+        try {
+            record = releaseHistoricalService.getIssuesByDateAndRelease(date, idRelease);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(record, HttpStatus.OK);
+    }
+}
