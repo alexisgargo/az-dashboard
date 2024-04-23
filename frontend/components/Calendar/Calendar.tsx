235b70e45b6c077d32bdafc0fc83a5c92fa7c2f8
Giuliana <gherreralpez@gmail.com>
2024-04-22 21:55:12 -0600

    AddButtonCalendar

--- frontend/components/Calendar/Calendar.tsx
+++ frontend/components/Calendar/Calendar.tsx
@@ -5,6 +5,7 @@ import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { Button } from '@nextui-org/button';
 
 export default function DateCalendarValue(setDate: any) {
   const [value, setValue] = React.useState<Dayjs | null>(dayjs()); 
@@ -17,12 +18,14 @@ export default function DateCalendarValue(setDate: any) {
   };
   
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DateCalendar', 'DateCalendar']}>
-        <DemoItem>
-          <DateCalendar value={value} onChange={handleDateChange} />
-        </DemoItem>
-      </DemoContainer>
-    </LocalizationProvider>
+   
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+          <DemoItem>
+            <DateCalendar value={value} onChange={handleDateChange} />
+          </DemoItem>
+        </DemoContainer>
+      </LocalizationProvider>
+    
   );
 }
