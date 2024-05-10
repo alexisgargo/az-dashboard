3281ac757e60cac30b228bbb5197ebaaa020ca53
alexis <alexisgargo3@gmail.com>
2024-05-09 20:15:34 -0600

    met-27: create release view

---
+++ frontend/components/releaseManagement/entryForm.tsx
@@ -0,0 +1,46 @@
+import { release } from "@/app/release/release.types";
+import { Input } from "@nextui-org/input";
+import { useEffect } from "react";
+
+export interface EntryFormProps {
+    label: string;
+    type: string;
+    release: release;
+    attribute: string;
+    setChanges: (release: release) => void;
+    required?: boolean;
+}
+
+export const EntryForm: React.FC<EntryFormProps> = (props) => {
+    /*
+        TODO: calendario 
+        const today = new Date();
+        const todayString = today.toISOString().split("T")[0];
+
+        defaultValue={props.type === "date" ? todayString : ""}
+    */
+    return (
+        <div className="w-full">
+            <Input
+                isRequired={props.required}
+                label={props.label}
+                type={props.type}
+                fullWidth
+                className="w-full"
+                placeholder={
+                    props.release[props.attribute]
+                        ? props.release[props.attribute]
+                        : props.type === "date"
+                        ? "YYYY-MM-DD"
+                        : ""
+                }
+                onValueChange={(e) => {
+                    props.setChanges({
+                        ...props.release,
+                        [props.attribute]: e,
+                    });
+                }}
+            />
+        </div>
+    );
+};
