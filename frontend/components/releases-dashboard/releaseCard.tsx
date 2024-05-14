b4d13c8e7400e3ccc7884ca2af57431f12dc5817
LionOrion0507 <alexleonc12@outlook.com>
2024-05-13 19:39:01 -0600

    MET-33 Modified card to show all teams progress, removed rollback, added arrow for design.

--- frontend/components/releases-dashboard/releaseCard.tsx
+++ frontend/components/releases-dashboard/releaseCard.tsx
@@ -1,12 +1,11 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { CircularProgress } from "@nextui-org/progress";
-import { Checkbox } from "@nextui-org/checkbox";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { useRouter } from "next/navigation";
 
 export default function ReleaseCard() {
   const router = useRouter()
   const is_hotfix = true
-  const is_rollback = true
 
   const onRedirect = () => {
     router.push('/release')
@@ -14,30 +13,36 @@ export default function ReleaseCard() {
 
   return (
     <Card isPressable onPress={click => onRedirect()}>
-      <CardHeader className="justify-center">
-        <h1 className="text-xl font-bold">Release Title</h1>
-      </CardHeader>
-      <CardBody className="flex flex-row justify-around space-x-5">
-        <CircularProgress
-          label="Release Progress"
-          color="success"
-          value={69}
-          minValue={0}
-          maxValue={100}
-          classNames={{ svg: "w-32 h-32", value: "text-3xl font-semibold" }}
-          showValueLabel={true}
-        />
-        <div>
-          <p>Release id: asdf1.2</p>
-          <p>Release status: on track</p>
-          <p>Code-cutoff date: yesterday</p>
-          <p>Release date: tomorrow</p>
-          <p>Release engineer: Lion</p>
-          <div className="flex flex-row gap-x-2">
-            <Checkbox isSelected={is_hotfix} isDisabled color="warning">Hotfix</Checkbox>
-            <Checkbox isSelected={is_rollback} isDisabled color="danger">Rollback</Checkbox>
+      <CardBody>
+        <div className="flex flex-row justify-around space-x-5">
+          <CircularProgress
+            color="success"
+            value={69}
+            minValue={0}
+            maxValue={100}
+            classNames={{ svg: "w-32 h-32", value: "text-3xl font-semibold" }}
+            showValueLabel={true}
+          />
+          <div className="content-center">
+            <h1 className="text-xl font-bold py-3">ReleaseName 1.2.3</h1>
+            <p>QA testing: 69%</p>
+            <p>UAT testing: 69%</p>
+            <p>PT testing: 69%</p>
+            <p>3rd Party testing: 69%</p>
           </div>
-          <p>Notes: this is pretty cool</p>
+        </div>
+        <div className="flex flex-row justify-between space-x-5 pt-5">
+          <div>
+            <p>Release status: on track</p>
+            <p>Code-cutoff date: yesterday</p>
+            <p>Release date: tomorrow</p>
+            <p>Release engineer: Lion</p>
+            <p>Release type: {is_hotfix ? "hotfix" : "normal"}</p>
+            <p>Notes: this is pretty cool</p>
+          </div>
+          <div className="content-end justify-end">
+            <ArrowForwardIosIcon fontSize="large" color="success" />
+          </div>
         </div>
       </CardBody>
     </Card>
