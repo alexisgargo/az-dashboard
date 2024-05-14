fa11af099aeaa55d9c2658b54f3da80d297e0536
TomasO <a01562785@tec.mx>
2024-05-14 17:43:29 -0600

    MAC-49 save every 5 min the release progress for each team from jira

--- backend/src/main/java/com/AZDash2/AzDash2Application.java
+++ backend/src/main/java/com/AZDash2/AzDash2Application.java
@@ -3,10 +3,12 @@ package com.AZDash2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableScheduling
 public class AzDash2Application {
 
 	public static void main(String[] args) {
