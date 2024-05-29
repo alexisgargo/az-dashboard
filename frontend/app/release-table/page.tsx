197655dd7493f61918050d6e1d57d2417bc97fb3
TomasO <a01562785@tec.mx>
2024-05-29 11:51:09 -0600

    All Releases filtered table

---
+++ frontend/app/release-table/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { FilteredTable } from "@/components/table/filteredTable";
+import { columns, filters, dateFilters } from "./release.types";
+import { Divider } from "@nextui-org/divider";
+import { getRows } from "./release.table.api";
+
+
+export default async function TestTablePage() {
+    const rows = await getRows();
+
+    return (
+        <div className="w-full">
+            <h1 className="text-3xl font-bold text-center">Releases Table</h1>
+            <Divider className="my-4" />
+            <FilteredTable
+                columns={columns}
+                rows={rows}
+                filters={filters}
+                dateFilters={dateFilters}
+            />
+        </div>
+    );
+}
