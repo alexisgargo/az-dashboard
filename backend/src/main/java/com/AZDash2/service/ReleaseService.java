867ac20c46eb183ce721fa62585d19629776806b
SalvadoRobles03 <99054999+SalvadoRobles03@users.noreply.github.com>
2024-05-29 12:37:02 -0600

    📦 NEW: MAC-61 | Save Historical when Save New Release

--- backend/src/main/java/com/AZDash2/service/ReleaseService.java
+++ backend/src/main/java/com/AZDash2/service/ReleaseService.java
@@ -1,29 +1,49 @@
 package com.AZDash2.service;
 
+import com.AZDash2.entity.Release;
+import com.AZDash2.entity.ReleaseHistorical;
+import com.AZDash2.repository.ReleaseHistoricalRepository;
+import com.AZDash2.repository.ReleaseRepository;
+import java.math.BigDecimal;
+import java.sql.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.AZDash2.entity.Release;
-import com.AZDash2.repository.ReleaseRepository;
 
 @Service
 public class ReleaseService {
 
-    @Autowired
-    ReleaseRepository releaseRepository;
+  @Autowired ReleaseRepository releaseRepository;
 
-    public Release getReleaseById(Long idRelease) {
-        return releaseRepository.findByIdRelease(idRelease);
-    }
+  @Autowired ReleaseHistoricalRepository releaseHistoricalRepository;
 
-    // private final ReleaseRepository releaseRepository;
+  public Release getReleaseById(Long idRelease) {
+    return releaseRepository.findByIdRelease(idRelease);
+  }
 
-    // @Autowired
-    // public ReleaseService(ReleaseRepository releaseRepository) {
-    //     this.releaseRepository = releaseRepository;
-    // }
+  // private final ReleaseRepository releaseRepository;
 
-    public Release saveRelease(Release release) {
-        return releaseRepository.save(release);
-    }
+  // @Autowired
+  // public ReleaseService(ReleaseRepository releaseRepository) {
+  //     this.releaseRepository = releaseRepository;
+  // }
+
+  public Release saveRelease(Release release) {
+    Release savedRelease = releaseRepository.save(release);
+
+    ReleaseHistorical releaseHistorical = new ReleaseHistorical();
+    releaseHistorical.setRelease(savedRelease);
+    releaseHistorical.setPercent_qa(BigDecimal.ZERO);
+    releaseHistorical.setPercent_uat(BigDecimal.ZERO);
+    releaseHistorical.setPercent_third_party(BigDecimal.ZERO);
+    releaseHistorical.setPercent_pt(BigDecimal.ZERO);
+
+    Date currentDate = new Date(System.currentTimeMillis());
+    Time currentTime = new Time(System.currentTimeMillis());
+    releaseHistorical.setRecordDate(currentDate);
+    releaseHistorical.setRecordTime(currentTime);
+
+    releaseHistoricalRepository.save(releaseHistorical);
+
+    return savedRelease;
+  }
 }
