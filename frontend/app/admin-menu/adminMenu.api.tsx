877c071d3cb06c65d2fa1e0941eaf0ec760ac3c1
Jesús Horacio <a01566851@tec.mx>
2024-05-30 00:52:15 -0600

    MET-44 fixed API call to get all releases for admin menu

--- frontend/app/admin-menu/adminMenu.api.tsx
+++ frontend/app/admin-menu/adminMenu.api.tsx
@@ -1,9 +1,9 @@
 import { release } from "../release/release.types";
 
-const API_URL = "http://localhost:8080/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getReleases(): Promise<release[]> {
-    const res = await fetch(`${API_URL}az_dashboard/releases`);
+    const res = await fetch(`${API_URL}/releases`);
     const data = await res.json();
     return data;
 }
