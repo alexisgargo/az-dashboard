acd60e6d5368c31c8bd2f5e37ca5ff5cdaa19d34
LionOrion0507 <alexleonc12@outlook.com>
2024-06-01 23:59:12 -0600

    MET-37: Created the yearly metrics component for the release dashboard

--- frontend/app/releases-dashboard/releases-dashboard.api.tsx
+++ frontend/app/releases-dashboard/releases-dashboard.api.tsx
@@ -1,11 +1,17 @@
 import { releaseProgress } from "./releases-dashboard.types";
 
-const API_URL = "http://localhost:8080/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getActiveReleasesProgress(
   date: string
 ): Promise<releaseProgress[]> {
   const response = await fetch(`${API_URL}az_dashboard/releases-historicals/${date}`);
+  const data = response.json();
+  return data;
+}
+
+export async function getYearMetrics(date: string): Promise<number[]> {
+  const response = await fetch(`${API_URL}releases/release-yearly-metrics/${date}`);
   const data = response.json();
   return data;
 }
---
+++ frontend/components/releases-dashboard/dashboard-bottom.tsx
@@ -0,0 +1,12 @@
+import YearMetrics from "./year-metrics";
+
+
+export default function DashboardBottom(prop: { metrics: number[] }) {
+  return (
+    <div className="grid grid-cols-4 absolute bottom-10">
+      <div className="col-span-2">
+        <YearMetrics metrics={prop.metrics} />
+      </div>
+    </div>
+  )
+}
