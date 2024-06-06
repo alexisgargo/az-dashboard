4e8ccaa0cfeee21d2006c675ef4cc4e8dafa9583
alexis <alexisgargo3@gmail.com>
2024-06-05 23:02:41 -0600

    met-51: passing date as parameter to issue tables

--- frontend/app/bug-table/[id]/[date]/page.tsx
+++ frontend/app/bug-table/[id]/[date]/page.tsx
@@ -3,16 +3,18 @@ import { Divider } from "@nextui-org/divider";
 import { columns, filters, dateFilters } from "../../bug-table.types";
 import { getBugs } from "../../bug-table.api";
 
-export default async function BugTablePage({params}: any) {
-    const bugs = await getBugs(params.name, params.version);
+export default async function BugTablePage({ params }: any) {
+    const bugs = await getBugs(params.id, params.date);
 
     return (
         <div className="w-full">
-            <h1 className="text-3xl font-bold text-center">Bugs {params.name} {params.version}</h1>
+            <h1 className="text-3xl font-bold text-center">
+                Bugs {params.name} {params.version}
+            </h1>
             <Divider className="my-4" />
             <FilteredTable
                 columns={columns}
-                rows={bugs}
+                rows={bugs.filter((bug) => bug.is_feature === false)}
                 filters={filters}
                 dateFilters={dateFilters}
                 rowKey="issue_number"
--- frontend/components/individual-release/tickets-issues.tsx
+++ frontend/components/individual-release/tickets-issues.tsx
@@ -3,41 +3,58 @@ import { Chip } from "@nextui-org/chip";
 import { Button } from "@nextui-org/button";
 import { useRouter } from "next/navigation";
 
-export default function TicketsIssuesCard(props: { ticketsCount: number, bugsCount: number, releaseName: string, releaseVersion: string }) {
-  const router = useRouter()
+export default function TicketsIssuesCard(
+    props: Readonly<{
+        ticketsCount: number;
+        bugsCount: number;
+        releaseId: number;
+        date: string;
+    }>
+) {
+    const router = useRouter();
 
-  const onRedirectBugs = (name: string, version: string) => {
-    router.push(`/bug-table/${name}/${version}`)
-  }
+    const onRedirectBugs = (id: number, date: string) => {
+        router.push(`/bug-table/${id}/${date}`);
+    };
 
-  const onRedirectIssues = (name: string, version: string) => {
-    router.push(`/issue-table/${name}/${version}`)
-  }
+    const onRedirectIssues = (id: number, date: string) => {
+        router.push(`/issue-table/${id}/${date}`);
+    };
 
-  return (
-    <div style={{padding:'30px'}}>
+    return (
+        <div style={{ padding: "30px" }}>
+            <Card className="w-64">
+                <CardHeader className="flex justify-center">
+                    <h1 className="text-xl">View Issues and Bugs</h1>
+                </CardHeader>
+                <CardBody>
+                    <Button
+                        variant="light"
+                        className="flex justify-between py-2"
+                        onPress={() =>
+                            onRedirectIssues(props.releaseId, props.date)
+                        }
+                    >
+                        <h1 className="text-small">Issues list</h1>
+                        <div>
+                            <Chip color="primary">{props.ticketsCount}</Chip>
+                        </div>
+                    </Button>
 
-      <Card className="w-64">
-        <CardHeader className="flex justify-center">
-          <h1 className="text-xl">View Issues and Bugs</h1>
-        </CardHeader>
-        <CardBody>
-          <Button variant="light" className="flex justify-between py-2" onPress={() => onRedirectIssues(props.releaseName, props.releaseVersion)}>
-            <h1 className="text-small">Issues list</h1>
-            <div>
-              <Chip color="primary">{props.ticketsCount}</Chip>
-            </div>
-          </Button>
-
-          <Button variant="light" className="flex justify-between py-2" onPress={() => onRedirectBugs(props.releaseName, props.releaseVersion)}>
-            <h1 className="text-small">Bugs list</h1>
-            <div>
-              <Chip color="primary">{props.bugsCount}</Chip>
-            </div>
-          </Button>
-        </CardBody>
-      </Card>
-
-    </div>
-  )
+                    <Button
+                        variant="light"
+                        className="flex justify-between py-2"
+                        onPress={() =>
+                            onRedirectBugs(props.releaseId, props.date)
+                        }
+                    >
+                        <h1 className="text-small">Bugs list</h1>
+                        <div>
+                            <Chip color="primary">{props.bugsCount}</Chip>
+                        </div>
+                    </Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
