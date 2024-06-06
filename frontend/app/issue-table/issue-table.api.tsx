1bc150e89ef1f56a565361510626ae1d78f2fbcb
alexis <alexisgargo3@gmail.com>
2024-06-05 22:59:15 -0600

    met-51: updated backend calls to match new apis

--- frontend/app/issue-table/issue-table.api.tsx
+++ frontend/app/issue-table/issue-table.api.tsx
@@ -2,12 +2,9 @@ import { issue } from "./issue-table.types";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function getIssues(
-    name: string,
-    version: string
-): Promise<issue[]> {
-    const res = await fetch(`${API_URL}/issue/${name}/${version}`);
+export async function getIssues(id: number, date: string): Promise<issue[]> {
+    console.log(`${API_URL}/az_dashboard/issues/${date}/${id}`);
+    const res = await fetch(`${API_URL}/az_dashboard/issues/${date}/${id}`);
     const data = await res.json();
     return data;
-
 }
