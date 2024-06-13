4e4c66e1e9e1a49ea3f0e1526ee2694256086722
alexisgargo <a01562707@tec.mx>
2024-06-12 23:17:09 -0600

    deleted remaining az_dashboard

--- frontend/app/releases-dashboard/releases-dashboard.api.tsx
+++ frontend/app/releases-dashboard/releases-dashboard.api.tsx
@@ -3,9 +3,17 @@ import { releaseProgress } from "./releases-dashboard.types";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getActiveReleasesProgress(
-  date: string
+    date: string
 ): Promise<releaseProgress[]> {
-  const response = await fetch(`${API_URL}/releases-historicals/${date}`);
-  const data = response.json();
-  return data;
+    const response = await fetch(`${API_URL}/releases-historicals/${date}`);
+    const data = response.json();
+    return data;
+}
+
+export async function getYearMetrics(date: string): Promise<number[]> {
+    const response = await fetch(
+        `${API_URL}/releases/release-yearly-metrics/${date}`
+    );
+    const data = response.json();
+    return data;
 }
--- frontend/components/individual-release/release-info.tsx
+++ frontend/components/individual-release/release-info.tsx
@@ -1,23 +1,30 @@
 import { release } from "@/app/release/release.types";
 import ProgressComponent from "./progress-component";
 
-export default function ReleaseInfo(prop: { releaseInfo: release, totalProgress: number}) {
-  return (
-    <div style={{display:'flex', alignItems:'center'}}>
-          <div>
-            <h1>Release: {prop.releaseInfo.name}</h1>
-            <p>Release ID: {prop.releaseInfo.id_release}</p>
-            <p>Release status: {prop.releaseInfo.status}</p>
-            <p>Code-cutoff date: {prop.releaseInfo.code_cutoff}</p>
-            <p>Release date: {prop.releaseInfo.curr_release_date}</p>
-            <p>Release engineer: {prop.releaseInfo.engineer.name}</p>
-            {prop.releaseInfo.is_hotfix ? <p>Hotfix</p> : <></>}
-            {prop.releaseInfo.is_rollback ? <p>Rolled back</p> : <></>}
-            <p>Notes: {prop.releaseInfo.release_note}</p>
-          </div>
-          <div>
-          <ProgressComponent totalProgress={prop.totalProgress} name={"Total Progress"}></ProgressComponent>
-          </div>
-    </div>
-  );
+export default function ReleaseInfo(prop: {
+    releaseInfo: release;
+    totalProgress: number;
+}) {
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <div>
+                <h1>
+                    Release: {prop.releaseInfo.name} {prop.releaseInfo.version}
+                </h1>
+                <p>Release ID: {prop.releaseInfo.id_release}</p>
+                <p>Release status: {prop.releaseInfo.status}</p>
+                <p>Code-cutoff date: {prop.releaseInfo.code_cutoff}</p>
+                <p>Release date: {prop.releaseInfo.curr_release_date}</p>
+                <p>Release engineer: {prop.releaseInfo.engineer.name}</p>
+                {prop.releaseInfo.is_hotfix ? <p>Hotfix</p> : <></>}
+                {prop.releaseInfo.is_rollback ? <p>Rolled back</p> : <></>}
+                <p>Notes: {prop.releaseInfo.release_note}</p>
+            </div>
+            <div>
+                <ProgressComponent
+                    totalProgress={prop.totalProgress}
+                    name={"Total Progress"}></ProgressComponent>
+            </div>
+        </div>
+    );
 }
